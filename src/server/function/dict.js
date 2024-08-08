1f4dce4887f497aba3ccbea9a8ff7b7528d8c3c1
gcat332 <kasira.cat@gmail.com>
2024-08-08 16:26:39 +0700

    update dictionary

--- src/server/function/dict.js
+++ src/server/function/dict.js
@@ -5,13 +5,13 @@ import { country } from "../dict/dict_country.js";
 import { district } from "../dict/dict_district.js";
 import { geo } from "../dict/dict_geo.js";
 import { ict } from "../dict/dict_ict.js";
-import { nhso } from "../dict/dict_nhso.js";
+import { universal } from "../dict/dict_universal.js";
 import { stopword } from "../dict/dict_stopword.js";
 
 export var dict = Array.from(
   new Set(
     collection.concat(
-      nhso,
+      universal,
       ict,
       acronyms,
       geo,
